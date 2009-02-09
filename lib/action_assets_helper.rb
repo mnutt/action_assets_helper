d0a769fd3ee44ac5f2ec5db4ac057c713b7b771f
Michael Nutt <michael@nuttnet.net>
2009-02-08 23:51:24 -0500

    Extracted from Limecast and others

---
+++ lib/action_assets_helper.rb
@@ -0,0 +1,19 @@
+module ActionAssetsHelper
+  def javascript_include(*scripts)
+    @javascript_includes ||= []
+    @javascript_includes << scripts
+  end
+  
+  def stylesheet_link(*stylesheets)
+    @stylesheet_links ||= []
+    @stylesheet_links << stylesheets
+  end
+
+  def javascript_include_actions
+    javascript_include_tag @javascript_includes if @javascript_includes
+  end
+
+  def stylesheet_link_actions
+    stylesheet_link_tag @stylesheet_links if @stylesheet_links
+  end
+end
---
+++ spec/action_assets_helper_spec.rb
@@ -0,0 +1,36 @@
+require File.dirname(__FILE__) + '/spec_helper'
+require 'action_view/helpers/asset_tag_helper'
+
+describe ActionAssetsHelper do
+  include ActionAssetsHelper
+  include ActionView::Helpers::TagHelper
+  include ActionView::Helpers::AssetTagHelper
+
+  it 'should include a single javascript asset from an action' do
+    javascript_include("posts_show")
+    javascript_include_actions.should == javascript_include_tag("posts_show")
+  end
+
+  it 'should include multiple javascript assets from an action' do
+    javascript_include("posts_show", "inline_editing")
+    javascript_include_actions.should == [javascript_include_tag("posts_show"), javascript_include_tag("inline_editing")].join("\n")
+  end
+
+  it 'should not include anything if no assets have been included' do
+    javascript_include_actions.should_not be
+  end
+
+  it 'should include a single css link from an action' do
+    stylesheet_link("posts_show")
+    stylesheet_link_actions.should == stylesheet_link_tag("posts_show")
+  end
+
+  it 'should include multiple css links from an action' do
+    stylesheet_link("posts_show", "show_forms")
+    stylesheet_link_actions.should == [stylesheet_link_tag("posts_show"), stylesheet_link_tag("show_forms")].join("\n")
+  end
+
+  it 'should not include anything if no css links given' do
+    stylesheet_link_actions.should_not be
+  end
+end
